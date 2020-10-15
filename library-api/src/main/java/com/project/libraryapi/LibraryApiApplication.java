b2c5302b018d15d8afcdf8fffeb5c840392af922
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2020-10-15 17:10:50 -0400

    Configuração para gerar WAR do projeto.

--- library-api/src/main/java/com/project/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/com/project/libraryapi/LibraryApiApplication.java
@@ -3,12 +3,13 @@ package com.project.libraryapi;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class LibraryApiApplication {
+public class LibraryApiApplication extends SpringBootServletInitializer {
 
     @Bean
     public ModelMapper modelMapper() {
--- spring-boot-admin/src/main/java/com/project/admin/AdminApplication.java
+++ spring-boot-admin/src/main/java/com/project/admin/AdminApplication.java
@@ -2,12 +2,13 @@ package com.project.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @EnableAdminServer
 @SpringBootApplication
-public class AdminApplication {
+public class AdminApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
